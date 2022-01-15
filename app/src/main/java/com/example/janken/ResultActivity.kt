0ef338640676e4a449724c728265e9d0d4564c78
tatsuya-ss <tatsu.knight.soccer@ezweb.ne.jp>
2022-01-15 17:35:38 +0900

    じゃんけんの処理を実装

--- app/src/main/java/com/example/janken/ResultActivity.kt
+++ app/src/main/java/com/example/janken/ResultActivity.kt
@@ -7,12 +7,16 @@ import com.example.janken.databinding.ActivityResultBinding
 
 class ResultActivity : AppCompatActivity() {
 
+    val gu = 0
+    val choki = 1
+    val pa = 2
+
     private lateinit var binding: ActivityResultBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setupBinding()
-        setupMyHandImage()
+        setupJanken()
     }
 
     fun setupBinding() {
@@ -20,12 +24,39 @@ class ResultActivity : AppCompatActivity() {
         setContentView(binding.root)
     }
 
-    fun setupMyHandImage() {
+    fun setupJanken() {
         val id = intent.getIntExtra("MY_HAND", 0)
-        when(id) {
-            R.id.gu -> binding.myHandImage.setImageResource(R.drawable.gu)
-            R.id.choki -> binding.myHandImage.setImageResource(R.drawable.choki)
-            R.id.pa -> binding.myHandImage.setImageResource(R.drawable.pa)
+        val myHand: Int
+        myHand = when(id) {
+            R.id.gu -> {
+                binding.myHandImage.setImageResource(R.drawable.gu)
+                gu
+            }
+            R.id.choki -> {
+                binding.myHandImage.setImageResource(R.drawable.choki)
+                choki
+            }
+            R.id.pa -> {
+                binding.myHandImage.setImageResource(R.drawable.pa)
+                pa
+            }
+            else -> gu
+        }
+
+        // コンピュータの手を決める
+        val comHand = (Math.random() * 3).toInt()
+        when(comHand) {
+            gu -> binding.comHandImage.setImageResource(R.drawable.com_gu)
+            choki -> binding.comHandImage.setImageResource(R.drawable.com_choki)
+            pa -> binding.comHandImage.setImageResource(R.drawable.com_pa)
+        }
+
+        // 勝敗を判定する
+        val gameResult = (comHand - myHand + 3) % 3
+        when(gameResult) {
+            0 -> binding.resultLabel.setText(R.string.result_draw)
+            1 -> binding.resultLabel.setText(R.string.result_win)
+            2 -> binding.resultLabel.setText(R.string.result_lose)
         }
     }
 
