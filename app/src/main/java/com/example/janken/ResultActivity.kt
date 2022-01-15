e999e9a5882ae07b233235e8da3f310ff1d5808d
tatsuya-ss <tatsu.knight.soccer@ezweb.ne.jp>
2022-01-15 18:16:22 +0900

    じゃんけんデータ保存処理

--- app/src/main/java/com/example/janken/ResultActivity.kt
+++ app/src/main/java/com/example/janken/ResultActivity.kt
@@ -2,6 +2,8 @@ package com.example.janken
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.content.edit
+import androidx.preference.PreferenceManager
 import com.example.janken.databinding.ActivityMainBinding
 import com.example.janken.databinding.ActivityResultBinding
 
@@ -20,12 +22,12 @@ class ResultActivity : AppCompatActivity() {
         setupBackButton()
     }
 
-    fun setupBinding() {
+    private fun setupBinding() {
         binding = ActivityResultBinding.inflate(layoutInflater)
         setContentView(binding.root)
     }
 
-    fun setupJanken() {
+    private fun setupJanken() {
         val id = intent.getIntExtra("MY_HAND", 0)
         val myHand: Int
         myHand = when(id) {
@@ -59,12 +61,45 @@ class ResultActivity : AppCompatActivity() {
             1 -> binding.resultLabel.setText(R.string.result_win)
             2 -> binding.resultLabel.setText(R.string.result_lose)
         }
+
+        // ゲームデータを保存する
+        saveData(myHand, comHand, gameResult)
     }
 
-    fun setupBackButton() {
+    private fun setupBackButton() {
         binding.backButton.setOnClickListener {
             // 現在のアクティビティを終了して、前のアクティビティに戻る
             finish()
+        }
+    }
+
+    private fun saveData(myHand: Int, comHand: Int, gameResult: Int) {
+        // 共有プリファレンスを取得
+        val pref = PreferenceManager.getDefaultSharedPreferences(this)
+        // 共有プリファレンスから値を取得して、変数に代入
+        val gameCount = pref.getInt("GAME_COUNT", 0)
+        val winningStreakCount = pref.getInt("WINNING_STREAK_COUNT", 0)
+        val lastComHand = pref.getInt("LAST_COM_HAND", 0)
+        val lastGameResult = pref.getInt("GAME_RESULT", -1)
+
+        // 連勝回数を保持する変数
+        val edtWinningStreakCount: Int =
+            when {
+                lastGameResult == 2 && gameResult == 2 ->
+                    winningStreakCount + 1
+                else ->
+                    0
+            }
+
+        pref.edit {
+            // 勝負回数をカウントアップ
+            putInt("GAME_COUNT", gameCount + 1)
+            // 共有プリファレンスに書き込み
+            putInt("WINNING_STREAK_COUNT", edtWinningStreakCount)
+            putInt("LAST_MY_HAND", myHand)
+            putInt("LAST_COM_HAND", comHand)
+            putInt("BEFORE_LAST_COM_HAND", lastComHand)
+            putInt("GAME_RESULT", gameResult)
         }
     }
 
