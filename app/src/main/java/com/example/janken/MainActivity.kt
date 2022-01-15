81322fe001e51f9a36813cc0b2ec80e94af4da3d
tatsuya-ss <tatsu.knight.soccer@ezweb.ne.jp>
2022-01-15 18:27:52 +0900

    データクリアメソッド

--- app/src/main/java/com/example/janken/MainActivity.kt
+++ app/src/main/java/com/example/janken/MainActivity.kt
@@ -4,6 +4,8 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import androidx.core.content.edit
+import androidx.preference.PreferenceManager
 import com.example.janken.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -16,15 +18,16 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setupBinding()
         setupButton()
+        clearData()
     }
 
-    fun setupBinding() {
+    private fun setupBinding() {
         // ビューバインディングの設定
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
     }
 
-    fun setupButton() {
+    private fun setupButton() {
         // ボタンの処理を設定
         binding.gu.setOnClickListener {
             onJankenButtonTapped(it)
@@ -38,12 +41,19 @@ class MainActivity : AppCompatActivity() {
     }
 
     // 引数はViewを受け取れるように指定
-    fun onJankenButtonTapped(view: View?) {
+    private fun onJankenButtonTapped(view: View?) {
         // 第1引数は呼び出し下のインスタンス
         // 第2引数はjava.lang.Classを指定する
         // この第2引数をKotlinでは「クラス名::class.java」と書く
         val resultsActivityIntent = Intent(this, ResultActivity::class.java)
         resultsActivityIntent.putExtra("MY_HAND", view?.id)
         startActivity(resultsActivityIntent)
+    }
+
+    private fun clearData() {
+        val pref = PreferenceManager.getDefaultSharedPreferences(this)
+        pref.edit {
+            clear()
+        }
     }
 }
